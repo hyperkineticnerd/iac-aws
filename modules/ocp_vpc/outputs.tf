960d1564f0d6a08cf5feec20b274bb79f40b86d6
Sean Nelson <hyperkineticnerd@gmail.com>
2024-09-17 20:40:17 -0700

    descriptions for variables and outputs

--- modules/ocp_vpc/outputs.tf
+++ modules/ocp_vpc/outputs.tf
@@ -1,11 +1,14 @@
 output "subnets" {
+  description = "List of Subnets for AZs"
   value = [aws_subnet.ocp_subnet_1a.id, aws_subnet.ocp_subnet_1b.id, aws_subnet.ocp_subnet_1c.id]
 }
 
 output "vpcs" {
+  description = "New VPC ID"
   value = aws_vpc.ocp_new_vpc.id
 }
 
 output "azs" {
+  description = "First 3 AZs for a given AWS Region"
   value = slice(data.aws_availability_zones.available.names, 0, 3)
 }
--- modules/ocp_vpc/variables.tf
+++ modules/ocp_vpc/variables.tf
@@ -1,12 +1,18 @@
 variable "vpc_tag" {
+  type = string
+  description = "AWS VPC Tag/Name"
   default = "OpenShift-Training"
 }
 
 variable "vpc_region" {
+  type = string
+  description = "AWS Region for VPC"
   default = "us-east-1"
 }
 
 variable "vpc_cidr" {
+  type = string
+  description = "CIDR for VPC"
   # default = "10.10.0.0/23" # main
   default = "10.10.2.0/23" # training
   # default = "10.10.4.0/23" # staging
@@ -15,6 +21,7 @@ variable "vpc_cidr" {
 
 variable "vpc_subnets_cidrs" {
   type = list(string)
+  description = "List of Subnet CIDRs for VPC Creation"
   default = [
     "10.10.2.64/26",
     "10.10.2.128/26",
@@ -23,6 +30,8 @@ variable "vpc_subnets_cidrs" {
 }
 
 variable "aws_service_base" {
+  type = string
+  description = "Service Name Suffix, in reverse fqdn notation"
   # default = "gov.sgov.sc2s"
   default = "com.amazonaws"
 }
